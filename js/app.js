4ab7bcd042d876cbd0ae1ee0bd162468bbe83001
Ibrahim Kasoma <kasomaibrahim@gmail.com>
2021-10-25 15:36:39 +0100

    checkLetter function and the keyboard event handerler

--- js/app.js
+++ js/app.js
@@ -2,7 +2,14 @@ const keyboard = document.querySelector('#qwerty');
 const phrase = document.getElementById('phrase');
 const startBtn = document.querySelector('.btn__reset');
 const ul = document.querySelector('ul');
-const buttons = document.querySelectorAll('button')
+const tries = document.querySelectorAll('.tries')
+
+// loop through the buttons
+// const button = () => {
+//     for(const btn of buttons){
+//         return btn
+//     }
+// }
 
 // All phrases
 const phrases = ['see eye to eye', 'the best of both worlds', 'speak of the devil', 'break a leg',  'blessing in disguise']
@@ -20,6 +27,7 @@ const getRandomPhraseAsArray = (arr) => {
     return arr[rand].split('');
 }
 const phraseArray = getRandomPhraseAsArray(phrases);
+
 // Loop of listItems of Characters
 const addPhraseToDisplay = (arr) => {
     for(let i = 0; i < arr.length; i++){
@@ -28,29 +36,45 @@ const addPhraseToDisplay = (arr) => {
         // ul.appendChild(listItem);
         phrase.firstElementChild.appendChild(listItem);
         if(listItem.textContent === " "){
-            listItem.classList.remove('letter');
+            listItem.classList.add('space');
         }else{
             listItem.classList.add('letter');
-            listItem.classList.add('show')
+           
         }
-        
-        return listItem;
     }
 }
 
-// const checkLetter = (selected) => {
-//     for(const btn of selected){
-//         const guesed = addPhraseToDisplay(getRandomPhraseAsArray(phrases))
-//         if(btn === guesed.textContent){
-//             guesed.classList.add('show')
-//         }
-//         return guesed
-//     }
-    
-// }
+const displayPhrase = addPhraseToDisplay(phraseArray);
 
-// keyboard.addEventListener('click', e => {
+const checkLetter = (btn) => {
+    const letters = document.querySelectorAll('.letter');
+    let match = '';
+    for(const letter of letters){
+        if(letter.innerHTML === btn.textContent){
+            match = letter.classList.add('show');
+        }else{
+            match = null;
+        }
+        
+    }
+    return match;
+ }
 
-//     console.log(checkLetter(buttons) )
-// })
 
+keyboard.addEventListener('click', e => {
+    const button = e.target;
+    if(button.tagName === "BUTTON"){
+        button.classList.add('chosen');
+        button.disabled = true;
+    }
+    const letterFound = checkLetter(button);
+    return letterFound;
+})
+
+const triesCount = () => {
+    if(letterFound === null){
+        for(const trial of tries){
+            trial.style.visibility = 'hidden';
+        }
+    }
+}
